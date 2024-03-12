1cf40dfc1fa3796037803b0e7817fbedec36df27
jul-ee <0813turara@naver.com>
2024-03-12 21:47:41 +0900

    Create HandButton for Button Printing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import HandIcon from './HandIcon';
+import HandButton from './HandButton';
 
-// value prop으로 각각 'rock', 'scissor', 'paper'를 넘겨준다
 function App() {
+  const handleClick = (value) => console.log(value);
   return(
     <div>
-      <HandIcon value="rock" />
-      <HandIcon value="scissor" />
-      <HandIcon value="paper" />
+      <HandButton value="rock" onClick={handleClick} />
+      <HandButton value="scissor" onClick={handleClick} />
+      <HandButton value="paper" onClick={handleClick} />
     </div>
   );
 }
--- src/HandIcon.js
+++ src/HandIcon.js
@@ -2,14 +2,12 @@ import rockImg from './assets/rock.svg';
 import scissorImg from './assets/scissor.svg';
 import paperImg from './assets/paper.svg';
 
-// IMAGES 객체를 이용해, value prop 값에 따라 이미지 주소를 다르게 지정한다
 const IMAGES = {
   rock: rockImg,
   scissor: scissorImg,
   paper: paperImg
 };
 
-// alt에는 value prop을 그대로 넣어준다
 function HandIcon({value}) {
   const src = IMAGES[value];
   return <img src={src} alt="rock"></img>;
