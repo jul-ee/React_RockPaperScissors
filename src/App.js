45f1b758bc0e563d7905b4b6ea59f964369ad23a
jul-ee <0813turara@naver.com>
2024-03-12 21:40:13 +0900

    Show images using props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import HandIcon from './HandIcon';
 
+// value prop으로 각각 'rock', 'scissor', 'paper'를 넘겨준다
 function App() {
   return(
     <div>
-      <HandIcon />
+      <HandIcon value="rock" />
+      <HandIcon value="scissor" />
+      <HandIcon value="paper" />
     </div>
   );
 }
