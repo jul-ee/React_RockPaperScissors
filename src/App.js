b9412fc9880e58f9f2b44e73bc2f31ad79d2381a
jul-ee <0813turara@naver.com>
2024-03-12 22:28:37 +0900

    Show the result by the score

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { compareHand, generateRandomHand } from './utils';
 
 const INITIAL_VALUE = 'rock';
 
-// 승리, 패배,  무승부에 해당하는 승부 결과를 만든다
 function getResult(me, other) {
   const comparison = compareHand(me, other);
   if (comparison > 0) return '승리';
@@ -17,35 +16,58 @@ function getResult(me, other) {
 function App() {
   const [hand, setHand] = useState(INITIAL_VALUE);
   const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
-  const [gameHistory, setGameHistory] = useState([]);    // 빈 배열을 초기 값으로 갖는 gameHistory State 생성
+  const [gameHistory, setGameHistory] = useState([]);
+  // score State, otherScore State, bet State 생성
+  const [score, setScore] = useState(0);
+  const [otherScore, setOtherScore] = useState(0);
+  const [bet, setBet] = useState(1);
 
   const handleButtonClick = (nextHand) => {
     const nextOtherHand = generateRandomHand();
-    const nextHistoryItem = getResult(nextHand, nextOtherHand);    // 승부 결과를 누적해주는 nextHistoryItem 변수 생성
+    const nextHistoryItem = getResult(nextHand, nextOtherHand);
+    const comparison = compareHand(nextHand, nextOtherHand);
     setHand(nextHand);
     setOtherHand(nextOtherHand);
-    // setGameHistory 함수로 값을 변경한다
-    // gameHistory State는 참조형이므로, Spread 문법으로 새로운 배열을 만들어 값을 변경해야 한다
     setGameHistory([...gameHistory, nextHistoryItem]);
+    // 승부 결과를 바탕으로 점수에 반영한다
+    if (comparison > 0) setScore(score + bet);
+    if (comparison < 0) setOtherScore(otherScore + bet);
   };
 
   const handleClearClick = () => {
     setHand(INITIAL_VALUE);
     setOtherHand(INITIAL_VALUE);
-    // 초기화 기능이므로, setGameHistory 함수에 초기 값인 빈 배열을 전달해준다
     setGameHistory([]);
+    // setScore, setOther, setBet의 초기 값 지정
+    setScore(0);
+    setOtherScore(0);
+    setBet(1);
   };
 
-  // join 메소드를 활용해, 승부 결과를 쉼표로 연결해서 보여준다
-  // 아규먼트로 전달한 값을 배열의 각 요소들 사이에 넣어서, 결과적으로는 하나의 문자열로 만들어주는 메소드
+  const handleBetChange = (e) => {
+    // 이벤트 핸들러애서 input의 value 속성을 참조하여, 이 값을 setBet 함수에 넘겨준다
+    // 이 값의 타입은 문자열이기 때문에, Number 생성자를 활용해 숫자형으로 변환한다
+    let num = Number(e.target.value);
+    // 나머지 연산과 내림 연산으로 1과 9 사이의 정수를 만들어준다
+    if (num > 9) num %= 10;
+    if (num < 1) num = 1;
+    num = Math.floor(num);
+    setBet(num);
+  };
+
   return(
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
-      <p>{getResult(hand, otherHand)}</p>
+      <div>
+        {score} : {otherScore}
+      </div>
       <div>
         <HandIcon value={hand} />
         VS
         <HandIcon value={otherHand} />
+      </div>
+      <div>
+        <input type="number" value={bet} min={1} max={9} onChange={handleBetChange}></input>
       </div>
       <p>승부 기록: {gameHistory.join(', ')}</p>
       <div>
