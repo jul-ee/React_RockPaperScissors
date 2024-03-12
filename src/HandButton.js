54feb752f1860302b9be14bfbf5a68b4676b7886
jul-ee <0813turara@naver.com>
2024-03-12 22:45:08 +0900

    Change the button color when rise the mouse

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,30 +1,12 @@
 import HandIcon from './HandIcon';
-import backgroundImg from './assets/purple.svg'
-
-// Button 프로퍼티에 인라인 스타일을 적용하기 위해 객체로 속성값을 지정해 준다
-const style = {
-  width: '166px',
-  height: '166px',
-  border: 'none',
-  outline: 'none',
-  textAlign: 'center',
-  cursor: 'pointer',
-  // 대시(-)로 여러 단어가 연결된 속성명은 CamelCase로 작성
-  backgroundColor: 'transparent',
-  // 이미지 주소는 import 구문을 통해서 가져온 값을 템플릿 문자열로 넣어준다
-  backgroundImage: `url('${backgroundImg}')`,
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  backgroundSize: 'contain',
-};
+import './HandButton.css';    // 기존의 스타일을 다루고 있던 객체들 대신, HandButton.css 파일을 import한다
 
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value);
-  
-  // Button 컴포넌트에 style 속성 지정
+
   return (
-    <button style={style} onClick={handleClick}>
-      <HandIcon value={value} />
+    <button className="HandButton" onClick={handleClick}>
+      <HandIcon className="HandButton-icon" value={value} />
     </button>
   );
 }
