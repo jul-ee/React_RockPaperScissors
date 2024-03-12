ed615543316662726af08fa2d9f0c65136b93ff2
jul-ee <0813turara@naver.com>
2024-03-12 22:37:17 +0900

    Apply design to the button

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
   const [hand, setHand] = useState(INITIAL_VALUE);
   const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
   const [gameHistory, setGameHistory] = useState([]);
-  // score State, otherScore State, bet State 생성
   const [score, setScore] = useState(0);
   const [otherScore, setOtherScore] = useState(0);
   const [bet, setBet] = useState(1);
@@ -29,7 +28,6 @@ function App() {
     setHand(nextHand);
     setOtherHand(nextOtherHand);
     setGameHistory([...gameHistory, nextHistoryItem]);
-    // 승부 결과를 바탕으로 점수에 반영한다
     if (comparison > 0) setScore(score + bet);
     if (comparison < 0) setOtherScore(otherScore + bet);
   };
@@ -38,17 +36,13 @@ function App() {
     setHand(INITIAL_VALUE);
     setOtherHand(INITIAL_VALUE);
     setGameHistory([]);
-    // setScore, setOther, setBet의 초기 값 지정
     setScore(0);
     setOtherScore(0);
     setBet(1);
   };
 
   const handleBetChange = (e) => {
-    // 이벤트 핸들러애서 input의 value 속성을 참조하여, 이 값을 setBet 함수에 넘겨준다
-    // 이 값의 타입은 문자열이기 때문에, Number 생성자를 활용해 숫자형으로 변환한다
     let num = Number(e.target.value);
-    // 나머지 연산과 내림 연산으로 1과 9 사이의 정수를 만들어준다
     if (num > 9) num %= 10;
     if (num < 1) num = 1;
     num = Math.floor(num);
--- src/HandButton.js
+++ src/HandButton.js
@@ -1,9 +1,29 @@
 import HandIcon from './HandIcon';
+import backgroundImg from './assets/purple.svg'
+
+// Button 프로퍼티에 인라인 스타일을 적용하기 위해 객체로 속성값을 지정해 준다
+const style = {
+  width: '166px',
+  height: '166px',
+  border: 'none',
+  outline: 'none',
+  textAlign: 'center',
+  cursor: 'pointer',
+  // 대시(-)로 여러 단어가 연결된 속성명은 CamelCase로 작성
+  backgroundColor: 'transparent',
+  // 이미지 주소는 import 구문을 통해서 가져온 값을 템플릿 문자열로 넣어준다
+  backgroundImage: `url('${backgroundImg}')`,
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: 'contain',
+};
 
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value);
+  
+  // Button 컴포넌트에 style 속성 지정
   return (
-    <button onClick={handleClick}>
+    <button style={style} onClick={handleClick}>
       <HandIcon value={value} />
     </button>
   );
